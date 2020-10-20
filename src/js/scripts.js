db89afb7cc6d17d5d3242af284d3c6cd9d5d81e3
Stanislav Chyrva <9104948@gmail.com>
2020-10-21 00:24:48 +0300

    test-task

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,17 @@
 console.log('hello');
+
+//SWIPER INITIALISATION ------------------------------------------------------
+
+window.addEventListener('DOMContentLoaded', initSwiper);
+
+function initSwiper() {
+    const mySwiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.main__swiper-button-next',
+            prevEl: '.main__swiper-button-prev',
+        },
+    });
+}
